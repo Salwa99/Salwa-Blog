363fe0f20d7e7146416e24fd11acb8331975249f
“Salwa99” <s.ritage99@gmail.com>
2023-04-19 15:47:35 +0000

    Generate the mmigration file for the Posts table

---
+++ db/migrate/20230419154708_create_posts.rb
@@ -0,0 +1,15 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.integer :author_id
+      t.string :title
+      t.text :text
+      t.timestamp :created_at
+      t.timestamp :updated_at
+      t.integer :comments_counter
+      t.integer :likes_counter
+
+      t.timestamps
+    end
+  end
+end
