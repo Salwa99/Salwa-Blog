a448a28dbbf396f731425a5f26e95e3c89d579bc
“Salwa99” <s.ritage99@gmail.com>
2023-04-20 15:28:19 +0000

    Update comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,10 +2,12 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  private
+
   def update_post_comments_counter
     post.comments_counter = post.comments.count
     post.save
   end
 
-  after_create :update_post_comments_counter
+  after_save :update_post_comments_counter
 end
