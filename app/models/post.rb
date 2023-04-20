3451a9135de6067cc7ab80d66d18a02ec7951ff6
“Salwa99” <s.ritage99@gmail.com>
2023-04-20 14:09:52 +0000

    Add recent comment  method to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,8 @@ class Post < ApplicationRecord
     author.posts_counter = author.posts.count
     author.save
   end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
