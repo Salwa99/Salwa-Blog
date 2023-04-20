95c5b3f00ab061b76a0fcc7afc658a41722a54e1
“Salwa99” <s.ritage99@gmail.com>
2023-04-20 14:14:59 +0000

    Add a  method to update the likes  in the like  model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,11 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  def update_post_likes_counter
+    post.likes_counter = post.likes.count
+    post.save
+  end
+
+  after_create :update_post_likes_counter
 end
