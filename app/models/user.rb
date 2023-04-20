8f39b130609b10a0cb14fc7ae94b473223d938ad
“Salwa99” <s.ritage99@gmail.com>
2023-04-20 13:30:43 +0000

    Update User  model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :comments, foreign_key: :author_id
-  has_many :likes, foreign_key: :author_id
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: "author_id"
+  has_many :likes, foreign_key: "author_id"
   validates :name, presence: true, uniqueness: true
 end
