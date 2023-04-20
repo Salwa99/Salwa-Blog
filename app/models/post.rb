1afd183d6cfdcb5fa98afe54abb8cd31dc945830
“Salwa99” <s.ritage99@gmail.com>
2023-04-20 15:24:57 +0000

    Update post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,14 +4,16 @@ class Post < ApplicationRecord
   has_many :likes
   validates :title, :text, presence: true
 
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
   def update_user_posts_counter
     author.posts_counter = author.posts.count
     author.save
   end
 
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
-
-  after_create :update_user_posts_counter
+  after_save :update_user_posts_counter, if: :saved_change_to_author_id?
 end
