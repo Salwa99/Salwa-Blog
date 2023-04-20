2f26feecc5ae0075b1c2238d76eb24951c16f8e1
“Salwa99” <s.ritage99@gmail.com>
2023-04-20 14:14:08 +0000

    Add a  method to update the comments in the comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  def update_post_comments_counter
+    post.comments_counter = post.comments.count
+    post.save
+  end
+
+  after_create :update_post_comments_counter
 end
