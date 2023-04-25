2decb3e19ad9ba926b7e0b4a587123ed93016562
Salwa99 <s.ritage99@gmail.com>
2023-04-25 20:00:42 +0100

    Update actions for post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,18 @@
 class PostsController < ApplicationController
-    def index
-    end
+  before_action :fetch_author
 
-    def show
-    end
+  def index
+    @posts = @author.posts
+  end
+
+  def show
+    @post = @author.posts.find(params[:post_id])
+  end
+
+  private
+
+  def fetch_author
+    @author = User.find(params[:user_id])
+  end
+
 end
