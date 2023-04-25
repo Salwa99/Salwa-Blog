8ad16768ec992e107ddc5bfe9cd83aa3c2c53cb1
Salwa99 <s.ritage99@gmail.com>
2023-04-25 19:36:25 +0100

    Update actions for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
-    def index
-    end
+  def index
+    @users = User.all
+  end
 
-    def show_user
-    end
+  def show
+    @user = User.find(params[:id])
+  end
 end
