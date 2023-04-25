41586284ef52d827bc9173c6e7305b51988ca372
Salwa99 <s.ritage99@gmail.com>
2023-04-25 19:22:43 +0100

    Add views to test routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
 root 'users#index'
 get '/user/:user_id', to: 'users#show_user'
 get '/user/:user_id/post', to: 'posts#index'
-get '/user/:user_id/post/:post_id', to: 'posts#show'
+get '/user/:user_id/post/:post_id', to: 'posts#show_post'
 end
