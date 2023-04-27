b486e33e4f8bf11233cddcc2589ec70a66ec485d
Salwa99 <s.ritage99@gmail.com>
2023-04-27 22:26:38 +0100

    Update routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show] do
-      resources :comments, only: %i[create]
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[new create]
+      resources :likes, only: %i[create]
     end
   end
 end
