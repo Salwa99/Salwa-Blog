a0ccb1faad77c4c045c5cc41708eaab1f1f72c82
Salwa99 <s.ritage99@gmail.com>
2023-04-28 15:00:44 +0100

    Add styling to views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,9 +19,7 @@ class CommentsController < ApplicationController
     end
   end
 
-  private
-
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
       resources :comments, only: %i[new create]
-      resources :likes, only: %i[create]
+      resources :likes, only: %i[create], as: :likes
     end
   end
 end
