1adb3cea7e5004f487daea5ddb208c3615cd0b88
Salwa99 <s.ritage99@gmail.com>
2023-04-28 15:01:51 +0100

    Fix the likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,6 +3,7 @@ class LikesController < ApplicationController
     @post = Post.find(params[:post_id])
     @like = Like.new(author: current_user, post: @post)
     if @like.save
+      @post.increment!(:likes_counter)
       flash[:success] = "Liked the #{@post.title}!"
     else
       flash[:error] = 'Failed to create like'
