90100ddb58d5047f247ce82b02c68379b8d6c507
Salwa99 <s.ritage99@gmail.com>
2023-05-03 22:08:12 +0100

    Migrate the db

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'user#index'
+  root 'users#index'
 
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
