11dbbd08eaa9d5f347b66ff7d06e26bb3eb3b0f4
Salwa99 <s.ritage99@gmail.com>
2023-05-05 11:31:33 +0100

    Fix Linter errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
   end
 
   def create
-      @post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @comment = current_user.comments.build(comment_params)
     @comment.post = @post
     if @comment.save
