2057291bfdb73c5fd47e5148d0100e159b2c429d
Salwa99 <s.ritage99@gmail.com>
2023-05-05 08:39:01 +0100

    Create api endpoint to list/show all comments

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::CommentsController < ApplicationController
+  before_action :fetch_post
+
+  def index
+    @comments = @post.comments
+    render json: @comments
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
+  def fetch_post
+    @post = Post.find_by(id: params[:post_id])
+    head :not_found if @post.nil?
+  end
+end
